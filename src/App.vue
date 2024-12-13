2b82ea460800e0cbde703293533092c3e1da6e89
Lulu <anlulu.dev@yahoo.com>
2024-12-13 15:54:15 +0800

    feat(App): 区分按键事件并优化界面布局
    - 在模板中添加 keypress 事件监听
    - 新增 keydownEvent 和 keypressEvent 变量分别记录不同事件
    - 更新界面布局，使事件显示更加清晰
    - 修改 onKeydown 和 onKeypress 函数，分别更新对应的事件变量

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <div class="card flex justify-center">
-    <InputText type="text" v-model="value" @keydown="onKeydown" />
-  </div>
-  <div>
-    {{ eventObject }}
-  </div>
+    <div class="card flex justify-center">
+        <InputText type="text" v-model="value" @keydown="onKeydown" @keypress="onKeypress" />
+    </div>
+    <div class="flex flex-col justify-center items-center gap-2 mt-8">
+        <div>keydown: {{ keydownEvent }}</div>
+        <div>keypress: {{ keypressEvent }}</div>
+    </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const value = ref(null);
 
-const eventObject = ref(null);
-
+const keydownEvent = ref(null);
+const keypressEvent = ref(null);
 const onKeydown = (event) => {
-  eventObject.value = JSON.stringify({ key: event.key, code: event.code });
+    keydownEvent.value = JSON.stringify({ key: event.key, code: event.code });
+};
+
+const onKeypress = (event) => {
+    keypressEvent.value = JSON.stringify({ key: event.key, code: event.code });
 };
 </script>
