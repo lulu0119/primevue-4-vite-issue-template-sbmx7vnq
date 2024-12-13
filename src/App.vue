f22824c694df4a761391d7641f92db5a20272216
Lulu <anlulu.dev@yahoo.com>
2024-12-13 18:31:19 +0800

    feat(App): 重构 App.vue 并添加新功能
    - 重新布局页面结构，使内容居中显示
    - 增加 InputNumber 组件，支持数字输入
    - 添加标签和改进样式，提升用户体验
    - 初始化 value 和 valueNumber 的值

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-    <div class="card flex justify-center">
-        <InputText type="text" v-model="value" @keydown="onKeydown" @keypress="onKeypress" />
+    <div class="card flex justify-center items-center flex-col gap-2">
+        <div class="flex flex-col gap-2">
+            <label for="input-text">Input Text</label>
+            <InputText
+                id="input-text"
+                type="text"
+                v-model="value"
+                @keydown="onKeydown"
+                @keypress="onKeypress"
+            />
+        </div>
+        <div class="flex flex-col gap-2">
+            <label for="input-number">Input Number</label>
+            <InputNumber
+                id="input-number"
+                type="number"
+                v-model="valueNumber"
+                @keydown="onKeydown"
+                @keypress="onKeypress"
+            />
+        </div>
     </div>
     <div class="flex flex-col justify-center items-center gap-2 mt-8">
-        <div>keydown: {{ keydownEvent }}</div>
-        <div>keypress: {{ keypressEvent }}</div>
+        <div>keydown:</div>
+        <div class="text-sm">{{ keydownEvent }}</div>
+        <div>keypress:</div>
+        <div class="text-sm">{{ keypressEvent }}</div>
     </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const value = ref(null);
+const value = ref("Hello World");
+const valueNumber = ref(2351.35);
 
 const keydownEvent = ref(null);
 const keypressEvent = ref(null);
