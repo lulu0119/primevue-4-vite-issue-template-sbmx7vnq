52f1ada145a1804effd609ed98b0a531351193e2
Lulu <anlulu.dev@yahoo.com>
2024-12-13 14:26:37 +0800

    chore: 初始化项目模板
    - 添加 .gitignore 文件，配置忽略的文件和目录
    - 添加 .vscode/extensions.json，推荐 Volar 扩展
    - 创建 README.md，提供项目说明和学习资源链接
    - 添加 index.html，创建基本的 HTML 结构

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card flex justify-center">
+    <InputText type="text" v-model="value" @keydown="onKeydown" />
+  </div>
+  <div>
+    {{ eventObject }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const value = ref(null);
+
+const eventObject = ref(null);
+
+const onKeydown = (event) => {
+  eventObject.value = JSON.stringify({ key: event.key, code: event.code });
+};
+</script>
